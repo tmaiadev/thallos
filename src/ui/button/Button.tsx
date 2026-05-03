7a4a17eaa03ab70484680b8997a8ccc4c2b06805
Thalles Maia <tmaiadev@gmail.com>
2026-05-03 11:30:20 +0100

    style: fixes indentation in Button component

--- src/ui/button/Button.tsx
+++ src/ui/button/Button.tsx
@@ -3,14 +3,14 @@ import styles from "./Button.module.css";
 import { cn } from "../../utils/cn";
 
 export function Button({ children, className, ref, ...props }: ButtonProps) {
-    return (
-        <button
-            className={cn(styles.button, className)}
-            type="button"
-            ref={ref}
-            {...props}
-        >
-            {children}
-        </button>
-    );
+  return (
+    <button
+      className={cn(styles.button, className)}
+      type="button"
+      ref={ref}
+      {...props}
+    >
+      {children}
+    </button>
+  );
 };
