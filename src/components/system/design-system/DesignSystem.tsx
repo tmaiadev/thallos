a4871d1dbb01dd8878a998d1d8e683397a37406f
Thalles Maia <tmaiadev@gmail.com>
2026-05-04 10:41:51 +0100

    feat: showcases Separator in DesignSystem and wraps Button in Fieldset

--- src/components/system/design-system/DesignSystem.tsx
+++ src/components/system/design-system/DesignSystem.tsx
@@ -4,24 +4,28 @@ import RadioButton from "@/components/ui/radio-button";
 import Fieldset from "@/components/ui/fieldset";
 import style from "./DesignSystem.module.css";
 import TextField from "@/components/ui/text-field";
+import Separator from "@/components/ui/separator";
 
 export function DesignSystem() {
   return (
     <div className={style.wrapper}>
       <h1>Thall<b>OS</b></h1>
       <h2>Components</h2>
-      <h3>Button</h3>
-      <Button>OK</Button>
-      <Button disabled>Disabled</Button>
+      <Fieldset legend="Button">
+        <Button>OK</Button>
+        <Button disabled>Disabled</Button>
+      </Fieldset>
       <Fieldset legend="Checkbox">
         <Checkbox>Normal checkbox</Checkbox>
         <Checkbox defaultChecked>Checked checkbox</Checkbox>
+        <Separator />
         <Checkbox disabled>Normal checkbox</Checkbox>
         <Checkbox defaultChecked disabled>Normal checkbox</Checkbox>
       </Fieldset>
       <Fieldset legend="Radio Buttons">
         <RadioButton name="option" value="0">Normal radio button</RadioButton>
         <RadioButton name="option" value="1" defaultChecked>Checked radio button</RadioButton>
+        <Separator />
         <RadioButton disabled>Disabled radio button</RadioButton>
         <RadioButton defaultChecked disabled>Checked disabled radio button</RadioButton>
       </Fieldset>
@@ -30,6 +34,7 @@ export function DesignSystem() {
         <TextField label="Read Only" placeholder="With Value" variant="inline" readOnly />
         <TextField label="Disabled Text Field" variant="inline" disabled />
         <TextField label="Disabled With Value" placeholder="With Value" variant="inline" disabled />
+        <Separator />
         <TextField label="Text Field" />
         <TextField label="Read Only" placeholder="With Value" readOnly />
         <TextField label="Disabled Text Field" disabled />
